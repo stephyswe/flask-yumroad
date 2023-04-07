becddf374690c7ddcb2667afbd293c6e6241619b
Stephanie <stephaniiehallberg@gmail.com>
2023-04-07 08:58:00 +0200

    feat(forms): create

--- yumroad/blueprints/products.py
+++ yumroad/blueprints/products.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, request, url_for, abort
 
-from yumroad.models import Product
+from yumroad.models import Product, db
+from yumroad.forms import ProductForm
 
 products = Blueprint('products', __name__)
 
@@ -8,6 +9,16 @@ products = Blueprint('products', __name__)
 def index():
     all_products = Product.query.all()
     return render_template('products/index.html', products=all_products)
+
+@products.route('/create', methods=['GET', 'POST'])
+def create():
+    form = ProductForm()
+    if form.validate_on_submit():
+        product = Product(name=form.name.data, description=form.description.data)
+        db.session.add(product)
+        db.session.commit()
+        return redirect(url_for('.details', product_id=product.id))
+    return render_template('products/new.html', form=form)
 
 @products.route('/<int:product_id>')
 def details(product_id):
--- yumroad/config.py
+++ yumroad/config.py
@@ -1,15 +1,19 @@
+import os
+
 class BaseConfig:
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv('YUMROAD_SECRET_KEY')
 
 class DevConfig(BaseConfig):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///dev.db'
-    SQLALCHEMY_ECHO = True
+    SECRET_KEY = os.getenv('YUMROAD_SECRET_KEY', '00000abcdef')
 
 class TestConfig(BaseConfig):
     TESTING = True
-    DEBUG = False
     SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
+    SECRET_KEY = os.getenv('YUMROAD_SECRET_KEY', '12345abcdef')
+    WTF_CSRF_ENABLED = False
 
 class ProdConfig(BaseConfig):
     DEBUG = False
